eec50bbdcc495607c3826dc017e2c2608b3d9c92
Tianyuan Fu <ftygary@gmail.com>
2019-11-30 20:09:51 -0800

    minor change

--- HMM.py
+++ HMM.py
@@ -12,16 +12,17 @@ state_sequence = []  # list of all training data
 test_sequence = []
 cpg_islands = []
 disjoint_prob = {}
-transition_prob = {'C+': {'C+': 0.37023851904592386, 'C-': 3.5599857600569602e-34, 'A+': 0.19366322534709862, 'A-': 3.5599857600569602e-34, 'T-': 3.5599857600569602e-34, 'T+': 0.1968672125311499, 'G-': 3.5599857600569602e-34, 'G+': 0.2392310430758277}, 'C-': {'C+': 0.0002288015375463323, 'C-': 0.3457648835400174, 'A+': 4.576030750926646e-35, 'A-': 0.2658216263213289, 'T-': 0.28645952500800803, 'T+': 4.576030750926646e-35, 'G-': 0.10172516359309934, 'G+': 4.576030750926646e-35}, 'A+': {'C+': 0.3181076672104405, 'C-': 8.156606851549756e-34, 'A+': 0.16557911908646003, 'A-': 8.156606851549756e-34, 'T-': 8.156606851549756e-34, 'T+': 0.11745513866231648, 'G-': 8.156606851549756e-34, 'G+': 0.3988580750407831}, 'A-': {'C+': 0.00042475728155339805, 'C-': 0.22554611650485437, 'A+': 6.067961165048544e-35, 'A-': 0.24939320388349515, 'T-': 0.18216019417475726, 'T+': 6.067961165048544e-35, 'G-': 0.3424757281553398, 'G+': 6.067961165048544e-35}, 'T-': {'C+': 0.00011155111829996096, 'C-': 0.28043951140610185, 'A+': 5.577555914998049e-35, 'A-': 0.1246025991410564, 'T-': 0.2654358859947571, 'T+': 5.577555914998049e-35, 'G-': 0.3294104523397847, 'G+': 5.577555914998049e-35}, 'T+': {'C+': 0.3731117824773414, 'C-': 7.552870090634441e-34, 'A+': 0.0649546827794562, 'A-': 7.552870090634441e-34, 'T-': 7.552870090634441e-34, 'T+': 0.17220543806646527, 'G-': 7.552870090634441e-34, 'G+': 0.38972809667673713}, 'G-': {'C+': 0.00028767320324111806, 'C-': 0.2658579853286666, 'A+': 4.7945533873519686e-35, 'A-': 0.20736443400297264, 'T-': 0.1873232008438414, 'T+': 4.7945533873519686e-35, 'G-': 0.33916670662127824, 'G+': 4.7945533873519686e-35}, 'G+': {'C+': 0.3431178103927013, 'C-': 0.0027766759222530744, 'A+': 0.15589051963506545, 'A-': 0.0007933359777865926, 'T-': 0.0007933359777865926, 'T+': 0.15827052756842522, 'G-': 0.0035700119000396666, 'G+': 0.3347877826259421}}
-# emission_prob = {'A+': {'A': 1.0, 'G': 0.0, 'C': 0.0, 'T': 0.0},
-#                  'A-': {'A': 1.0, 'G': 0.0, 'C': 0.0, 'T': 0.0},
-#                  'G+': {'A': 0.0, 'G': 1.0, 'C': 0.0, 'T': 0.0},
-#                  'G-': {'A': 0.0, 'G': 1.0, 'C': 0.0, 'T': 0.0},
-#                  'C+': {'A': 0.0, 'G': 0.0, 'C': 1.0, 'T': 0.0},
-#                  'C-': {'A': 0.0, 'G': 0.0, 'C': 1.0, 'T': 0.0},
-#                  'T+': {'A': 0.0, 'G': 0.0, 'C': 0.0, 'T': 1.0},
-#                  'T-': {'A': 0.0, 'G': 0.0, 'C': 0.0, 'T': 1.0}}
-emission_prob = {'C+': {'A': 1e-30, 'C': 1.0, 'T': 1e-30, 'G': 1e-30}, 'C-': {'A': 1e-30, 'C': 1.0, 'T': 1e-30, 'G': 1e-30}, 'A+': {'A': 1.0, 'C': 1e-30, 'T': 1e-30, 'G': 1e-30}, 'A-': {'A': 1.0, 'C': 1e-30, 'T': 1e-30, 'G': 1e-30}, 'T-': {'A': 1e-30, 'C': 1e-30, 'T': 1.0, 'G': 1e-30}, 'T+': {'A': 1e-30, 'C': 1e-30, 'T': 1.0, 'G': 1e-30}, 'G-': {'A': 1e-30, 'C': 1e-30, 'T': 1e-30, 'G': 1.0}, 'G+': {'A': 1e-30, 'C': 1e-30, 'T': 1e-30, 'G': 1.0}}
+# transition_prob = {'C+': {'C+': 0.37023851904592386, 'C-': 3.5599857600569602e-34, 'A+': 0.19366322534709862, 'A-': 3.5599857600569602e-34, 'T-': 3.5599857600569602e-34, 'T+': 0.1968672125311499, 'G-': 3.5599857600569602e-34, 'G+': 0.2392310430758277}, 'C-': {'C+': 0.0002288015375463323, 'C-': 0.3457648835400174, 'A+': 4.576030750926646e-35, 'A-': 0.2658216263213289, 'T-': 0.28645952500800803, 'T+': 4.576030750926646e-35, 'G-': 0.10172516359309934, 'G+': 4.576030750926646e-35}, 'A+': {'C+': 0.3181076672104405, 'C-': 8.156606851549756e-34, 'A+': 0.16557911908646003, 'A-': 8.156606851549756e-34, 'T-': 8.156606851549756e-34, 'T+': 0.11745513866231648, 'G-': 8.156606851549756e-34, 'G+': 0.3988580750407831}, 'A-': {'C+': 0.00042475728155339805, 'C-': 0.22554611650485437, 'A+': 6.067961165048544e-35, 'A-': 0.24939320388349515, 'T-': 0.18216019417475726, 'T+': 6.067961165048544e-35, 'G-': 0.3424757281553398, 'G+': 6.067961165048544e-35}, 'T-': {'C+': 0.00011155111829996096, 'C-': 0.28043951140610185, 'A+': 5.577555914998049e-35, 'A-': 0.1246025991410564, 'T-': 0.2654358859947571, 'T+': 5.577555914998049e-35, 'G-': 0.3294104523397847, 'G+': 5.577555914998049e-35}, 'T+': {'C+': 0.3731117824773414, 'C-': 7.552870090634441e-34, 'A+': 0.0649546827794562, 'A-': 7.552870090634441e-34, 'T-': 7.552870090634441e-34, 'T+': 0.17220543806646527, 'G-': 7.552870090634441e-34, 'G+': 0.38972809667673713}, 'G-': {'C+': 0.00028767320324111806, 'C-': 0.2658579853286666, 'A+': 4.7945533873519686e-35, 'A-': 0.20736443400297264, 'T-': 0.1873232008438414, 'T+': 4.7945533873519686e-35, 'G-': 0.33916670662127824, 'G+': 4.7945533873519686e-35}, 'G+': {'C+': 0.3431178103927013, 'C-': 0.0027766759222530744, 'A+': 0.15589051963506545, 'A-': 0.0007933359777865926, 'T-': 0.0007933359777865926, 'T+': 0.15827052756842522, 'G-': 0.0035700119000396666, 'G+': 0.3347877826259421}}
+transition_prob = {}
+emission_prob = {
+                'C+': {'A': 1e-30, 'C': 1.0, 'T': 1e-30, 'G': 1e-30}, 
+                'C-': {'A': 1e-30, 'C': 1.0, 'T': 1e-30, 'G': 1e-30}, 
+                'A+': {'A': 1.0, 'C': 1e-30, 'T': 1e-30, 'G': 1e-30}, 
+                'A-': {'A': 1.0, 'C': 1e-30, 'T': 1e-30, 'G': 1e-30}, 
+                'T-': {'A': 1e-30, 'C': 1e-30, 'T': 1.0, 'G': 1e-30}, 
+                'T+': {'A': 1e-30, 'C': 1e-30, 'T': 1.0, 'G': 1e-30}, 
+                'G-': {'A': 1e-30, 'C': 1e-30, 'T': 1e-30, 'G': 1.0}, 
+                'G+': {'A': 1e-30, 'C': 1e-30, 'T': 1e-30, 'G': 1.0}}
 
 def load_sequence_data():
     # read sequence data
@@ -41,7 +42,6 @@ def load_test_data():
         for state in line:
             if state != '\n':
                 test_sequence.append(state)
-
 def load_cpg_data():
     # read cpg island positions
     f = open(CPG_POSITION_PATH, 'r')
@@ -52,95 +52,82 @@ def load_cpg_data():
     # done reading island positions
 
 def calculate_probability():
-    count = {'C+': 2809, 'C-': 21853, 'A+': 1226, 'A-': 16480, 'T-': 17929, 'T+': 1324, 'G-': 20857, 'G+': 2521}
-    total = 0
-    for key in count:
-        total += count[key]
-    for key in count:
-        disjoint_prob[key] = count[key] / total
+    disjoint_count = {}
+    transition_count = {}
+    # initialize local variable
+    for state in STATES:
+        disjoint_count[state] = 0
+    for state in STATES:
+        temp = {}
+        for state_2 in STATES:
+            temp[state_2] = 0
+            transition_count[state] = temp
+    # done initializing
+
+    # get counts
+    index = 0
+    for island in cpg_islands:
+        # start_index = int(island[0]) - 1
+        # end_index = int(island[1]) - 1
+                
+        start_index = int(island[0])
+        end_index = int(island[1])
+        while index < start_index:
+            # disjoint count
+            state = state_sequence[index] + '-'
+            disjoint_count[state] += 1
+            # transition count
+            if index + 1 != start_index:
+                next_state = state_sequence[index + 1] + '-'
+            else:
+                next_state = state_sequence[index + 1] + '+'
+            transition_count[state][next_state] += 1
+            # update index
+            index += 1
+        while index <= end_index:
+            # disjoint count
+            state = state_sequence[index] + '+'
+            disjoint_count[state] += 1
+            # transition count
+            if index != end_index:
+                next_state = state_sequence[index + 1] + '+'
+            else:
+                next_state = state_sequence[index + 1] + '-'
+            transition_count[state][next_state] += 1
+            # update index
+            index += 1
+    while index < len(state_sequence):
+        state = state_sequence[index] + '-'
+        disjoint_count[state] += 1
+        if index != len(state_sequence) - 1:
+            next_state = state_sequence[index + 1] + '-'
+            transition_count[state][next_state] += 1
+        index += 1
+
+    print(disjoint_count)
+    print(transition_count)
+
+    # calculate disjoint probability
+    for state in disjoint_count:
+        disjoint_prob[state] = disjoint_count[state] / len(state_sequence)
+    # calculate transition probability
+    state_transition = {}  # total transition count for each state
+    for state in transition_count:
+        state_transition[state] = 0
+        for next_state in transition_count[state]:
+            state_transition[state] += transition_count[state][next_state]
+
+    for state in transition_count:
+        temp = {}
+        for next_state in transition_count[state]:
+            temp[next_state] = transition_count[state][next_state] / state_transition[state]
+            transition_prob[state] = temp
 
     print(disjoint_prob)
     print(sum(disjoint_prob.values()))
     print(transition_prob)
-    print(count)
+    print(sum(disjoint_count.values()))
     print(len(state_sequence))
-# def calculate_probability():
-#     disjoint_count = {}
-#     transition_count = {}
-#     # initialize local variable
-#     for state in STATES:
-#         disjoint_count[state] = 0
-#     for state in STATES:
-#         temp = {}
-#         for state_2 in STATES:
-#             temp[state_2] = 0
-#             transition_count[state] = temp
-#     # done initializing
-
-#     # get counts
-#     index = 0
-#     for island in cpg_islands:
-#         # start_index = int(island[0]) - 1
-#         # end_index = int(island[1]) - 1
-                
-#         start_index = int(island[0])
-#         end_index = int(island[1])
-#         while index < start_index:
-#             # disjoint count
-#             state = state_sequence[index] + '-'
-#             disjoint_count[state] += 1
-#             # transition count
-#             if index + 1 != start_index:
-#                 next_state = state_sequence[index + 1] + '-'
-#             else:
-#                 next_state = state_sequence[index + 1] + '+'
-#             transition_count[state][next_state] += 1
-#             # update index
-#             index += 1
-#         while index <= end_index:
-#             # disjoint count
-#             state = state_sequence[index] + '+'
-#             disjoint_count[state] += 1
-#             # transition count
-#             if index != end_index:
-#                 next_state = state_sequence[index + 1] + '+'
-#             else:
-#                 next_state = state_sequence[index + 1] + '-'
-#             transition_count[state][next_state] += 1
-#             # update index
-#             index += 1
-#     while index < len(state_sequence):
-#         state = state_sequence[index] + '-'
-#         disjoint_count[state] += 1
-#         if index != len(state_sequence) - 1:
-#             next_state = state_sequence[index + 1] + '-'
-#             transition_count[state][next_state] += 1
-#         index += 1
-
-#     print(disjoint_count)
-#     print(transition_count)
-
-#     # calculate disjoint probability
-#     for state in disjoint_count:
-#         disjoint_prob[state] = disjoint_count[state] / len(state_sequence)
-#     # calculate transition probability
-#     state_transition = {}  # total transition count for each state
-#     for state in transition_count:
-#         state_transition[state] = 0
-#         for next_state in transition_count[state]:
-#             state_transition[state] += transition_count[state][next_state]
-
-#     for state in transition_count:
-#         temp = {}
-#         for next_state in transition_count[state]:
-#             temp[next_state] = transition_count[state][next_state] / state_transition[state]
-#             transition_prob[state] = temp
-
-    # print(disjoint_prob)
-    # print(sum(disjoint_prob.values()))
-    # print(transition_prob)
-    # print(sum(disjoint_count.values()))
-    # print(len(state_sequence))
 
 def viterbi(observed_sequence):
     N = len(STATES)
